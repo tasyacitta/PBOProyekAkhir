ca77fc38be733c1b79096873a6abfcc8ce58539b
tasyacitta <81207865+tasyacitta@users.noreply.github.com>
2021-07-07 15:11:45 +0700

    Update RegisterView.java

--- BrainEatGame/src/view/RegisterView.java
+++ BrainEatGame/src/view/RegisterView.java
@@ -1,5 +1,6 @@
-package view;
 
+package View;
+import view.*;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.event.ActionEvent;
@@ -9,26 +10,32 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
-public class RegisterView extends JFrame implements ActionListener{
-    private JTextField fieldnama, fieldpw, fieldkonfirm;
-    private JLabel labeljudul, labelnama, labelpw, labelkonfirm;
-    private JButton btnregister, btnbatal;
-    
-    public void formRegister() {
-    setTitle("Register Account");
+public class RegisterView extends JFrame implements ActionListener {
+    public static void main(String[] args) {
+        RegisterView rv = new RegisterView();
+        rv.formRegis();
+    }
+    private JTextField fieldnama, fieldpw,fieldconfirm;
+    private JLabel labeljudul, labelnama, labelpw, labelconfirm;
+    private JButton btnbatal, btnregister;
+
+public void formRegis() {
+    setTitle("Registration");
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    setSize(220,225);
+    setSize(300,225);
     
-    labeljudul = new JLabel(" Register Account ");
-    labelnama = new JLabel(" Userame ");
+    labeljudul = new JLabel("Register Account ");
+    labelnama = new JLabel(" Username ");
     fieldnama = new JTextField(18);
     labelpw = new JLabel(" Password ");
     fieldpw = new JTextField(6);
-    labelkonfirm = new JLabel(" Konfirmasi Password ");
-    fieldkonfirm = new JTextField(6);
+    labelconfirm = new JLabel("Confirm Password ");
+    fieldconfirm = new JTextField(6);
     
-   
+    
+    btnbatal = new JButton(" Batal ");
     btnregister = new JButton(" Register ");
     
     setLayout(null);
@@ -37,34 +44,37 @@ public class RegisterView extends JFrame implements ActionListener{
     add(fieldnama);
     add(labelpw);
     add(fieldpw);
-    add(labelkonfirm);
-    add(fieldkonfirm);
-    add(btnregister);
+    add(labelconfirm);
+    add(fieldconfirm);
     add(btnbatal);
+    add(btnregister);
     
     labeljudul.setBounds(85,10,120,20);
     labelnama.setBounds(10,45,120,20);
-    fieldnama.setBounds(100,45,90,20);
-    labelpw.setBounds(10,100,120,20);
-    fieldpw.setBounds(100,100,90,20);
-    labelkonfirm.setBounds(10,100,120,20);
-    fieldkonfirm.setBounds(100,100,90,20);
-    btnregister.setBounds(12,130,90,20);
-    btnregister.setBackground(Color.blue);
-    btnregister.setForeground(Color.white);
-    btnregister.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-    //btnlogin.addActionListener(this);
-    btnbatal.setBounds(106,130,90,20);
+    fieldnama.setBounds(150,45,90,20);
+    labelpw.setBounds(10,70,120,20);
+    fieldpw.setBounds(150,70,90,20);
+    labelconfirm.setBounds(10,90,120,20);
+    fieldconfirm.setBounds(150,90,90,20);
+    btnbatal.setBounds(150,125,90,20);
     btnbatal.setBackground(Color.red);
     btnbatal.setForeground(Color.white);
     btnbatal.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+    //btnlogin.addActionListener(this);
+    btnregister.setBounds(30,125,90,20);
+    btnregister.setBackground(Color.blue);
+    btnregister.setForeground(Color.white);
+    btnregister.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
     //btnregister.addActionListener(this);
     
     setResizable(false);
     setLocation(450,200);
     setVisible(true);
     
+
+    
 }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==btnregister) {
@@ -72,12 +82,9 @@ public class RegisterView extends JFrame implements ActionListener{
                 setMessage("Username tidak boleh kosong");
                 }
             if(fieldpw.getText().equals("")){
-                setMessage("Password harus diisi");
+                setMessage("Massa barang harus diisi");
                 }
-            if(fieldkonfirm.getText().equals("")){
-                setMessage("Konfirmasi Password harus diisi");
-                }
-            if(fieldpw.getText().equals(fieldkonfirm.getText())) {
+            if(fieldpw.getText().equals(fieldconfirm.getText())){              
                   String[] data = {
                            fieldnama.getText(),
                            fieldpw.getText()
@@ -86,13 +93,15 @@ public class RegisterView extends JFrame implements ActionListener{
 //                //fungsiinputkedatabase
                 }
             else{
-             setMessage("Password dan Konfirmasi Password harus benar");
+                 setMessage("Harap Konfimasi dengan Benar!");
             }
         }
         
-        if(e.getSource()==btnregister) {
+        if(e.getSource()==btnbatal) {
            
+            dispose();
         }
+        
     }
 
     public void setMessage(String message) {
