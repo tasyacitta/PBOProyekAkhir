9c48b4315c010a6e754f378305fff65f797b74ac
dewizunuvi <82432170+dewizunuvi@users.noreply.github.com>
2021-07-07 14:23:05 +0700

    Update MenuAdminView.java

--- BrainEatGame/src/view/MenuAdminView.java
+++ BrainEatGame/src/view/MenuAdminView.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
-public class MenuAdminView {
+public class MenuAdminView extends JFrame implements ActionListener {
     private JLabel lJudul;
     private JButton bData;
     
