0f577608abb8d3636a60e84f4a8286780e466983
dewizunuvi <82432170+dewizunuvi@users.noreply.github.com>
2021-07-10 21:30:39 +0700

    Add files via upload

---
+++ BrainEatGame/src/main/Main.java
@@ -0,0 +1,16 @@
+/*
+Raden Rara Lydia Devina Syantasyacitta // Dewi Zunuvi Setiawati
+123190086                             // 123190117
+Plug IF-C
+*/
+package main;
+
+import controller.*;
+
+public class Main {
+    public static void main(String[] args){
+        ControlGame cg= new ControlGame(); //implimentasi polymorphysm
+        cg.openMenu();
+        
+    }
+}
